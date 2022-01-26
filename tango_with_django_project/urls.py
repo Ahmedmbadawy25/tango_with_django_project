27a4b8e38006e70c6673872773e0d8e3f670d36d
Ahmed Badawy <2613099b@student.gla.ac.uk>
2022-01-26 13:11:16 +0000

    "Chapter 3 commit with exercise completed"

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    #static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
 ]
